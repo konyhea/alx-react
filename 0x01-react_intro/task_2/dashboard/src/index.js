cb8b39de97633837b30b7054937f268c08360baa
Konyhea <ndukaprincek@gmail.com>
2024-11-20 01:57:37 +0100

    fixed

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Notifications from './Notifications';
 
-// Render the App component in the "root" div
 const rootApp = ReactDOM.createRoot(document.getElementById('root'));
 rootApp.render(
   <React.StrictMode>
@@ -11,7 +10,6 @@ rootApp.render(
   </React.StrictMode>
 );
 
-// Render the Notifications component in the "root-notifications" div
 const rootNotifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
 rootNotifications.render(
   <React.StrictMode>
