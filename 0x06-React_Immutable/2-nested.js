ecb5600ce969712915a13ff544beb1ed63dc9c6e
Konyhea <ndukaprincek@gmail.com>
2025-01-23 02:35:49 +0100

    getIn

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,20 +1,14 @@
+import {getIn} from 'immutable';
+
 /**
- * Get the value from an object using a defined path (array of keys).
+ * get the value from an object using getIn.
  * @param {Object} object - The plain JavaScript object.
- * @param {Array} array - An array of keys representing the path.
- * @returns {*} The value from the object at the specified path, or undefined if not found.
+ * @returns value from an object
  */
+
+
 export default function accessImmutableObject(object, array) {
-    let result = object;
-
-    for (let i = 0; i < array.length; i++) {
-        if (result && result.hasOwnProperty(array[i])) {
-            result = result[array[i]];  // Traverse down the path
-        } else {
-            return undefined;  // Return undefined if path does not exist
-        }
-    }
-
-    return result;  // Return the value at the end of the path
+        return getIn([object, array]);
 }
 
+ChatGPT
