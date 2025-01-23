ff34334f6c4043d23a3eb3ea7636a92c7d1e54de
Konyhea <ndukaprincek@gmail.com>
2025-01-23 01:54:39 +0100

    immutable

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,12 @@
+import {fromJS} from 'immutable';
+
+
+/**
+ * Converts a plain JavaScript object into an Immutable.js Map.
+ * @param {Object} object - The plain JavaScript object.
+ * @returns {Map} - Immutable.js Map representation of the object.
+ */
+
+export function getImmutableObject(object) {
+	return fromJS(object);
+}
